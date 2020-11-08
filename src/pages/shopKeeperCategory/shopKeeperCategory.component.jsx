8770a52e8aac65196da0791ea9bb11ac074dec21
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2020-11-08 20:50:26 +0600

    the shopkeeper pages

---
+++ src/pages/shopKeeperCategory/shopKeeperCategory.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./shopKeeperCategory.styles.scss";
+
+import FormInput from "../../components/form-input/form-input.component";
+
+const shopKeeperCategory= () => {
+  return (
+    <div>
+      <h1>This is the shop keeper category page</h1>
+      <div>
+        <h1>Food</h1>
+      </div>
+      <div>
+        <h1>+</h1>
+      </div>
+    </div>
+  );
+};
+
+export default shopKeeperCategory;
---
+++ src/pages/shopKeeperProduct/shopKeeperProduct.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./shopKeeperProduct.styles.scss";
+
+import FormInput from "../../components/form-input/form-input.component";
+
+const shopKeeperProduct = () => {
+  return (
+    <div>
+      <h1>This is the shop keeper Product page</h1>
+      <div>
+        <h1>Potato</h1>
+      </div>
+      <div>
+        <h1>+</h1>
+      </div>
+    </div>
+  );
+};
+
+export default shopKeeperProduct;

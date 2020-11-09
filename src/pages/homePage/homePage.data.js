a4fee775220d288e85ed0c634c535346903670e6
Sazid <initialsaremsu@gmail.com>
2020-11-09 21:13:13 +0600

    Fixed filename case issue and some css

--- src/pages/homePage/homePage.data.js
+++ src/pages/homePage/homePage.data.js
@@ -14,6 +14,11 @@ const ShopList = [
     name: "Crown Clothing",
     imageLink: "https://pbs.twimg.com/profile_images/675395159689949184/wOYPVW0l.jpg",
   },
+  {
+    id: 4,
+    name: "Crown Clothing",
+    imageLink: "https://pbs.twimg.com/profile_images/675395159689949184/wOYPVW0l.jpg",
+  },
 ];
 
 export default ShopList;
